1c3e82b436a32420128f27dd8299138f6e26c93a
Pierre Martin <pierremartin.pro@gmail.com>
2016-10-04 15:19:01 +0200

    Refactoring entry point

---
+++ app/configureStore.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
+
+const configureStore = () => {
+    const persistedState = loadState();
+    const store = createStore(todoApp, persistedState);
+    console.log(store.getState());
+
+    // fonction appelé à chaque fois que l'etat du localstorage change - 'throttle' va résoudre le probleme de chargement tres répété :
+    store.subscribe(throttle(() => {
+        saveState({
+            todos: store.getState().todos
+        });
+    }), 1000);
+
+    return store;
+};
+
+export default configureStore;
--- app/index.js
+++ app/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoApp from './reducers';
-import App from './components/App';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
-const store = createStore(todoApp, persistedState);
-console.log(store.getState());
-
-// fonction appelé à chaque fois que l'etat du localstorage change - 'throttle' va résoudre le probleme de chargement tres répété :
-store.subscribe(throttle(() => {
-   saveState({
-       todos: store.getState().todos
-   });
-}), 1000);
+const store = configureStore();
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Root store={store} />,
     document.getElementById('root')
 );
