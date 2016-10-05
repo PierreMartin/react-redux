0a7557bd677c344b0a36c6dee775f0779a1ca3ff
Pierre Martin <pierremartin.pro@gmail.com>
2016-10-05 22:40:59 +0200

    add logs (console.group)

--- app/configureStore.js
+++ app/configureStore.js
@@ -3,10 +3,32 @@ import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 import throttle from 'lodash/throttle';
 
+
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+
+    if (!console.group) {
+        return rawDispatch;
+    }
+
+    return (action) => {
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    };
+};
+
 const configureStore = () => {
     const persistedState = loadState();
     const store = createStore(todoApp, persistedState);
-    // console.log(store.getState());
+
+    if (process.env.NODE_ENV !== 'production') {
+        store.dispatch = addLoggingToDispatch(store);
+    }
 
     // fonction appelé à chaque fois que l'etat du localstorage change - 'throttle' va résoudre le probleme de chargement tres répété :
     store.subscribe(throttle(() => {
