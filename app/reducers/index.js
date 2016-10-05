3ca684550dae796a74540364f8901fc4947581f4
Pierre Martin <pierremartin.pro@gmail.com>
2016-10-05 18:20:31 +0200

    refactorisation function getVisibleTodos

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
-import todos from './todos';
+import todos, * as fromTodos from './todos';  // reducers
 
 const todoApp = combineReducers({
     todos
 });
 
 export default todoApp;
+
+
+// chargement des autres fonctions :
+export const getVisibleTodos = (state, filter) => (
+    fromTodos.getVisibleTodos(state.todos, filter)
+);
